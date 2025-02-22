dc404337afb54de72bc49cdb664ff774fb0b093f
Robert Bui <robert.bui404@gmail.com>
2025-02-22 17:42:22 +1100

    refactor: Remove Hero component from main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="font-[family-name:var(--font-geist-sans)]">
       <main>
-        <Hero />
+   
         <Tutorials id="tutorials" />
       </main>
     </div>
